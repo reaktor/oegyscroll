a21152a129c436e5bca7fafb13cd2308c3e3f4af
Juha Paananen <juha.paananen@reaktor.fi>
2009-08-25 12:28:40 +0000

    LARGE DEMO DATASET OF 1 MILLION ROWS !!

--- oegyscroll-demo/src/main/java/org/laughingpanda/oegyscrolldemo/OegyScrollDemoPage.java
+++ oegyscroll-demo/src/main/java/org/laughingpanda/oegyscrolldemo/OegyScrollDemoPage.java
@@ -1,46 +1,78 @@
 package org.laughingpanda.oegyscrolldemo;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.markup.html.AjaxLink;
 import org.apache.wicket.markup.html.WebMarkupContainer;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.repeater.data.ListDataProvider;
+import org.apache.wicket.markup.repeater.data.IDataProvider;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
 import org.laughingpanda.wicket.LazyLoadScrollableList;
 
 public class OegyScrollDemoPage extends WebPage {
-    static int counter = 0;
+    private final class SampleDataProvider implements
+			IDataProvider<String> {
+		private final int size;
 
-    private static List<String> sampleData() {
-        int count = ++counter;
-        List<String> data = new ArrayList<String>();
-        for (int i = 0; i < 21111; i++) {
-            data.add("Row " + i + " of dataset " + count);
-        }
-        return data;
-    }
+		private SampleDataProvider(int size) {
+			this.size = size;
+		}
+
+		public Iterator<? extends String> iterator(final int first, final int count) {
+			return new Iterator<String>() {
+				int index = first;
+
+				public boolean hasNext() {
+					return index < first + count;
+				}
+
+				public String next() {
+					try {
+						return "Row " + index + " of sample data set";						
+					} finally {
+						index++;
+					}
+				}
+
+				public void remove() {
+					throw new UnsupportedOperationException();
+				}};
+		}
+
+		public IModel<String> model(String object) {
+			return new Model<String>(object);
+		}
+
+		public int size() {
+			return size;
+		}
+
+		public void detach() {
+		}
+	}
+
+	static int counter = 0;
+
 
     public OegyScrollDemoPage() {
-        this(sampleData(), 100);
+        this(1000000, 500);
     }
 
-    public OegyScrollDemoPage(final List<String> data, final int blockSize) {
-        final LazyLoadScrollableList<String> scroller = new LazyLoadScrollableList<String>("djuizyScroller", new ListDataProvider<String>(data), blockSize) {
+    public OegyScrollDemoPage(final int size, final int blockSize) {
+        final LazyLoadScrollableList<String> scroller = new LazyLoadScrollableList<String>("djuizyScroller", new SampleDataProvider(size), blockSize) {
             @Override
             protected void populateRow(final WebMarkupContainer rowContainer, final int index, final String modelObject) {
                 rowContainer.add(new Label("rowLabel", new Model<String>(modelObject)));
             }
         };
         add(scroller);
+        
         add(new AjaxLink<Object>("refresh") {
             @Override
             public void onClick(final AjaxRequestTarget target) {
-                data.clear();
-                data.addAll(sampleData());
                 target.addComponent(scroller);
             }
         });
