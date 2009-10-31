189d5bb3edf6da69b3669c67dae25cbae29c02b5
Juha Paananen <juha.paananen@reaktor.fi>
2009-10-31 20:14:17 +0000

    OegyScroll client-side library kicks ass

--- oegyscroll/src/test/js/oegyscroll.js
+++ oegyscroll/src/test/js/oegyscroll.js
@@ -3,13 +3,19 @@ function OegyScroll(height, rowHeight, blockSize) {
 	this.rowHeight = rowHeight;
 	this.blockSize = blockSize;
 	this.blockHeight = blockSize * rowHeight;
-	this.main = $('<div id="scroller" style="height:'+height+'px;overflow:auto;border:1px solid #ccc;"></div>');
+	this.main = $('<div style="height:'+height+'px;overflow:auto;border:1px solid #ccc;"></div>');
+	this.main.attr("id", "scroller");	
 	this.scrollable = $('<div class="scrollable-area"></div>').appendTo(this.main);
 	this.contentArea = $('<table id="content"></table>').appendTo(this.scrollable);
 	this.createBlock = createBlock;
 	this.createPlaceholder = createPlaceholder;
 	this.createRow = createRow;
-	this.createPlaceHolderBlock = createPlaceHolderBlock;						
+	this.createPlaceHolderBlock = createPlaceHolderBlock;
+	this.createAutomaticPlaceholder = createAutomaticPlaceholder;	
+	this.scrollTo=scrollTo;	
+	this.init = init;	
+	this.refresh = refresh;	
+	this.autorefresh = autorefresh;		
 }	
 function createBlock(contentCreator) {
 	var block = $("<tbody></tbody>")
@@ -30,4 +36,55 @@ function createPlaceHolderBlock(id, content, onclickFunction) {
 	});
 	placeHolder.get(0).onclick=onclickFunction;
 	return block;
-};						
+};
+function init(rowCount, rowFetcher, placeHolderContent) {
+	var oegy = this;
+	this.remainder = rowCount % this.blockSize;
+	if (this.remainder == 0) this.remainder = this.blockSize;
+	this.createBlock(function(block) {
+		appendRowsToBlock(oegy, block, 0, oegy.remainder, rowFetcher);
+	});
+	this.blockCount = (rowCount - this.remainder) / this.blockSize;
+	for (i = 0; i < this.blockCount; i++) {
+		this.createAutomaticPlaceholder(i, rowFetcher, placeHolderContent);
+	}
+}
+function createAutomaticPlaceholder(blockNumber, rowFetcher, content) {
+	var oegy = this;
+	this.createPlaceHolderBlock('placeholder-' + blockNumber, content, function() {
+		replacePlaceholderWithData(oegy, $(this), blockNumber, rowFetcher);
+	});
+}
+
+function replacePlaceholderWithData(oegy, placeholder, blockNumber, rowFetcher) {
+	block = placeholder.parent();
+	offset = blockNumber * oegy.blockSize;
+	start = offset + oegy.remainder;
+	end = offset + oegy.blockSize + oegy.remainder;
+	appendRowsToBlock(oegy, block, start, end, rowFetcher);
+	placeholder.remove();
+}
+
+function appendRowsToBlock(oegy, block, start, end, rowFetcher) {
+	for (row = start; row < end; row++) {
+		oegy.createRow(rowFetcher(row)).attr("id", "row-" + (row+1)).appendTo(block);
+	}
+}
+function scrollTo(yPos) {
+	this.main.attr('scrollTop', yPos);
+};		
+
+function refresh() {
+	refreshPlaceholders(this.main.get(0), this.contentArea.get(0));
+}
+
+function autorefresh() {
+	updateRepeatedly(this.main.attr("id"));
+}
+
+function updateRepeatedly(id) {
+	main = $("div#" + id);
+	contentArea = $("table#content", main);
+	refreshPlaceholders(main.get(0), contentArea.get(0));
+	setTimeout('updateRepeatedly("' + id + '")', 1000);
+}
