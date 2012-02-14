eaa1b9c8a1b19c894eedb52c75eaddf21f8de7e8
Kalle Kainu <kalle.kainu@reaktor.fi>
2012-02-14 14:26:09 +0200

    Update wicket to 1.5.4

--- oegyscroll/src/test/java/org/laughingpanda/wicket/LazyLoadScrollableListSpec.java
+++ oegyscroll/src/test/java/org/laughingpanda/wicket/LazyLoadScrollableListSpec.java
@@ -23,7 +23,7 @@ import jdave.junit4.JDaveRunner;
 import jdave.wicket.ComponentSpecification;
 
 @RunWith(JDaveRunner.class)
-public class LazyLoadScrollableListSpec extends ComponentSpecification<LazyLoadScrollableListTestPage> {
+public class LazyLoadScrollableListSpec extends ComponentSpecification<LazyLoadScrollableListTestPage, Void> {
     List<String> testData = emptyList();
 
     private void createTestData(final int rowCount) {
