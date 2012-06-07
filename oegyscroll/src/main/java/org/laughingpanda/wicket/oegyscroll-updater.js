cf1192d9a800c59611ced4a94928782951cd8d9f
tanele <ext-tanel.ehrenpreis@elisa.fi>
2012-06-07 17:37:25 +0300

    Bugfix, first visible block location was sometimes calculated wrongly, causing the list to freeze without loading scrolled location or anything at all.

--- oegyscroll/src/main/java/org/laughingpanda/wicket/oegyscroll-updater.js
+++ oegyscroll/src/main/java/org/laughingpanda/wicket/oegyscroll-updater.js
@@ -75,8 +75,13 @@ OegyScrollUpdater.prototype.getFirstVisibleBlockStartingFrom = function(from) {
 	while (from > 0 && (!this.getBlock(from) || this.scrollPos() < this.blockPosition(from))) {
 		from -= 1;
 	}
+	var addingActivated;
 	while (this.scrollPos() > this.blockPosition(from) + this.blockHeight()) {
 		from += 1;
+		addingActivated = true;
+	}
+	if(addingActivated) {
+		from -= 1;
 	}
 	return from;
 }
