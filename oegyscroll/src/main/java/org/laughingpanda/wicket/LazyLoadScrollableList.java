2fd89530a5b65d50423ef60bdc70eec603ebb870
Nikolai Muhhin <nikolai.muhhin@tieto.com>
2012-07-23 14:59:06 +0300

    Fix methods name

--- oegyscroll/src/main/java/org/laughingpanda/wicket/LazyLoadScrollableList.java
+++ oegyscroll/src/main/java/org/laughingpanda/wicket/LazyLoadScrollableList.java
@@ -78,7 +78,7 @@ public abstract class LazyLoadScrollableList<T extends Serializable> extends Web
     private void addContentLoaderInitializationJavascript(IHeaderResponse response) {
         final String scrollerId = getMarkupId();
         final String scrolledContentId = get("scrolledContent").getMarkupId();
-        response.renderOnDomReadyJavascript("if (!window.oegyscroll) { window.oegyscroll = {timers: []}};" +
+        response.renderOnDomReadyJavaScript("if (!window.oegyscroll) { window.oegyscroll = {timers: []}};" +
         		"window.oegyscroll.timers['"+scrollerId+"'] = new OegyScrollUpdater(\""+scrollerId+"\", \""+scrolledContentId+"\").scheduleScrollPositionUpdate();");
     }
 }
