dcfae17ef3ef8b271b9f2deda3fb68a9cf1ee198
Timo Rantalaiho <timo.rantalaiho@reaktor.fi>
2012-07-24 12:05:26 +0300

    Revert "Store interval's identificator"
    This reverts commit f6057e82b0c3df1cb39486e349110b81dd25f8a7.

--- oegyscroll/src/main/java/org/laughingpanda/wicket/LazyLoadScrollableList.java
+++ oegyscroll/src/main/java/org/laughingpanda/wicket/LazyLoadScrollableList.java
@@ -80,7 +80,6 @@ public abstract class LazyLoadScrollableList<T extends Serializable> extends Web
     private void addContentLoaderInitializationJavascript(IHeaderResponse response) {
         final String scrollerId = getMarkupId();
         final String scrolledContentId = get("scrolledContent").getMarkupId();
-        response.renderOnDomReadyJavascript("if (!window.oegyscroll) { window.oegyscroll = {timers: []}};" +
-        		"window.oegyscroll.timers['"+scrollerId+"'] = new OegyScrollUpdater(\""+scrollerId+"\", \""+scrolledContentId+"\").scheduleScrollPositionUpdate();");
+        response.renderOnDomReadyJavaScript("new OegyScrollUpdater(\"" + scrollerId + "\", \"" + scrolledContentId + "\").scheduleScrollPositionUpdate();");
     }
 }
